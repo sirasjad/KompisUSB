4604c7f9e2315656c666855db723731b8de95e7c
Sirajuddin Asjad <sirasjad@gmail.com>
2018-05-30 20:41:59 +0200

    UI fix

--- Linux/KompisUSB/kompis.cpp
+++ Linux/KompisUSB/kompis.cpp
@@ -1,14 +1,14 @@
 #include "kompis.h"
 #include "ui_kompis.h"
 
-Kompis::Kompis(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::Kompis)
-{
+Kompis::Kompis(QWidget *parent) : QMainWindow(parent), ui(new Ui::Kompis){
     ui->setupUi(this);
 }
 
-Kompis::~Kompis()
-{
+Kompis::~Kompis(){
     delete ui;
+}
+
+void Kompis::on_startKompis_clicked(){
+    //ui->checkBox->setStyleSheet("QCheckBox { color: green; }");
 }
--- Linux/KompisUSB/main.cpp
+++ Linux/KompisUSB/main.cpp
@@ -1,8 +1,7 @@
 #include "kompis.h"
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     QApplication a(argc, argv);
     Kompis w;
     w.show();
